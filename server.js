42a4ffafe23a97cc3e69998e1aa55a0a756a8d64
jkxngr <botirovj781@gmail.com>
2024-11-30 22:45:45 +0500

    bug fix

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://task4-mu-jet.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+    credentials: true,
   })
 );
 
